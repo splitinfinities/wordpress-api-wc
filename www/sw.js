fc1055f01111f36034a60160f1c6ad1874a74bd7
William M. Riley <mikeriley20@gmail.com>
2018-01-05 12:44:05 -0600

    Ugh, oops

--- www/sw.js
+++ www/sw.js
@@ -20,11 +20,15 @@ importScripts('workbox-sw.prod.v2.1.2.js');
 const fileManifest = [
   {
     "url": "build/wordpress-api.js",
-    "revision": "d8a2a6bdf50413a4ee7230f83ab7e975"
+    "revision": "e8689d74f8f3b1bcf21c55c5508ba845"
   },
   {
-    "url": "build/wordpress-api/n4tovou7.js",
-    "revision": "b18f1bbd94a001d560f9e7e60fad80de"
+    "url": "build/wordpress-api/6dufn9g7.js",
+    "revision": "d002bd835e440e14d8fc360af218fad2"
+  },
+  {
+    "url": "build/wordpress-api/pig6k23h.js",
+    "revision": "c7923595dfc77204517ea4cc53b1c6ab"
   },
   {
     "url": "build/wordpress-api/wordpress-api.5t2ohrol.js",
@@ -40,15 +44,11 @@ const fileManifest = [
   },
   {
     "url": "build/wordpress-api/wordpress-api.registry.json",
-    "revision": "63feb0b0f4178cb26650e048e0cc322f"
-  },
-  {
-    "url": "build/wordpress-api/xeugkddn.js",
-    "revision": "1481b63b0ccca848056dc3df8b681da5"
+    "revision": "f464d280eed9a71b7761777b3b51aec6"
   },
   {
     "url": "index.html",
-    "revision": "80bbdb375865903adc6d313ea0a56b0b"
+    "revision": "de184d72206258c3e38008e0222b8df8"
   }
 ];
 
